027225c4edf64ee4932d08525ebad849f1a74902
Arthur DANJOU <arthurdanjou@outlook.fr>
2020-06-05 11:47:36 +0200

    fixing

--- components/home/passions.js
+++ components/home/passions.js
@@ -23,22 +23,6 @@ const PassionsHome = () => {
                         </p>
                     </div>
                 </div>
-                <div className="passions-bottom">
-                    <div className="passions-left passion">
-                        <h1>Le Développement</h1>
-                        <p>
-                            Je suis développeur Web FullStack, c'est à dire que je maîtrise le BackEnd et le FrontEnd.
-                            Je réalise des sites pour des projets personnels ou professionnels. En parallèle, je développe des logiciels mobiles et bureautiques. Je développe également des scripts pour la sécurité de mes applications et je réalise des tests d'intrusion pour tester l'efficacité de ma sécurité
-                        </p>
-                    </div>
-                    <div className="passions-right passion">
-                        <h1>La Technologie</h1>
-                        <p>
-                            J'adore le DevSecOps, c'est un mixe de développement, de sécurité et d'opérations qui consistent à automatiser le développement donc à gagner du temps, et les machines, en effet, j'aimerais concevoir une infrastructure propre à mes besoins que l'héberge et que je déploie moi même..
-                            Ce sont des domaines très importants qu'il ne faut pas négliger sur un projet ! Je me documente au quotidien pour respecter la vie privée de mes clients.
-                        </p>
-                    </div>
-                </div>
             </div>
         </section>
     )
